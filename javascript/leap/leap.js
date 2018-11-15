776a9c0885c553829f855ab309bd06c10ac83fd7
Ricardo Kreyhsig <rickreyhsig@gmail.com>
2018-11-15 09:55:38 -0500

    Removing spaghetti code from leap year

--- javascript/leap/leap.js
+++ javascript/leap/leap.js
@@ -1,5 +1,8 @@
-export const isLeap = (year) => {
+/*export const isLeap = (year) => {
   if (year % 4 == 0 && year % 100 != 0 ) return true;
   else if (year % 100 == 0 && year % 400 == 0) return true;
   return false;
-};
+};*/
+
+export const isLeap = (year) =>
+      ((year % 4) === 0 && ((year % 100) !== 0 || (year % 400) === 0));
